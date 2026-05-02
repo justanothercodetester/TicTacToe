d7b7c77ced1c4337edb457c8f075d78b81b6ae2f
justanothercodetester <ishaansinghsus@outlook.com>
2026-05-02 14:36:18 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module org.multitool {
+    requires atlantafx.base;
+    requires java.datatransfer;
+    requires java.desktop;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+
+    exports org.TicTacToe;
+    opens org.TicTacToe to javafx.fxml;
+}
